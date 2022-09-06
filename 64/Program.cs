c9199fa2620917020a83ab9fd0748ca2d911f6a8
dsail <dsail@yandex.ru>
2022-09-06 22:17:59 +0300

    example 64 commit

---
+++ 64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64.
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+void PrintNaturalNumbers(int numberM, int numberN)
+{
+    if (numberM == numberN)
+    Console.Write($"{numberN}");
+    else
+    {
+            Console.Write($"{numberM}, ");
+            PrintNaturalNumbers(numberM +1, numberN);
+    }
+}
+    Console.Write("Введите пожалуйста значение M = ");
+    int numberM = Convert.ToInt16(Console.ReadLine());
+
+    Console.Write("Введите пожалуйста значение N = ");
+    int numberN = Convert.ToInt16(Console.ReadLine());
+
+    Console.Write("Натуральные числа в промежутке от M до N: ");
+PrintNaturalNumbers(numberM, numberN);
